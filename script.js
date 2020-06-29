a26c2fe778feb77cf8a780286ee45488c105c8fb
ArthurLuis <alfa@cin.ufpe.br>
2020-06-29 13:12:44 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,51 @@
+const cvs = document.getElementById('canvas')
+
+const ctx = cvs.getContext('2d')
+
+//unidade
+
+const box = 32;
+
+//importar as imagens
+
+const fundo = new Image ();
+fundo.scr = "img/fundo.png";
+
+const lampadaImg = new Image ();
+lampadaImg.scr = "img/lampada.png";
+
+//cobra
+
+let snake = [];
+snake[0] =  {x: 9 * box, y: 10 * box};
+
+//lampadas
+let lampada = {
+    x : Math.floor (Math.random()* 17 + 1) * box,
+    y : Math.floor(Math.random() * 15 + 3) * box
+}
+
+//score
+let = score = 0;
+
+// draw  
+function draw(){
+    ctx.drawImage(fundo,0,0);
+    for( let i = 0; i < snake.length; i++){
+        ctx.fillStyle = (i == 0) ? "blue" : "white";
+        ctx.fillRect(snake[i].x, snake[i].y, box, box);
+        ctx.strokeStyle= "purple";
+        ctx.stokeRect(snake[i].x, snaike[i].y, box, box);
+    }
+    ctx.fillStyle="white";
+    ctx.font="45px Changa One"
+    ctx.fillText( score,2*box,1.6*box);
+}
+
+ctx.drawImage(lampadaImg, lampada.x, lampada.y);
+
+
+
+
+
+let game = setInterval (draw, 100);
